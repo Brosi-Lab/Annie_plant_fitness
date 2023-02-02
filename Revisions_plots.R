8af0491f529f2df34262baeab84b04d4babb41dc
Annie Schiffer <annie.schiffer808@gmail.com>
2023-02-02 16:14:15 -0700

    finishing touches

--- Revisions_plots.R
+++ Revisions_plots.R
@@ -1,14 +1,17 @@
+#### Supplementary Figures
 
+# load packages
 library(ggplot2)
 library(scales)
 
+# load data-- must have run Phenology_Plant_Fitness.Rmd first!!
 mert.counts.by.site$date<-as.Date(mert.counts.by.site$date)
 delph.counts.by.site$date<-as.Date(delph.counts.by.site$date)
 pot.counts.by.site$date<-as.Date(pot.counts.by.site$date)
 
-### By plot treatment
+### Plotting floral abundance by plot treatment
 
-#plotting Mertensia flower counts 
+# plotting Mertensia flower counts 
 ggplot(mert.counts.by.site,aes(x=date,y=number.conspecifics,group=plot.treat)) +
   theme_classic() +
   labs(y="Floral Abundance",x="Week",title = "Mertensia Peak Bloom") +
@@ -21,7 +24,7 @@ ggplot(mert.counts.by.site,aes(x=date,y=number.conspecifics,group=plot.treat)) +
                limits = as.Date(c('2019-06-07','2019-07-18')),
                labels = date_format(format = "%W"))
 
-#plotting Delphinium flower counts
+# plotting Delphinium flower counts
 ggplot(delph.counts.by.site,aes(x=date,y=number.conspecifics,group=plot.treat)) +
   theme_classic() +
   labs(y="Floral Abundance",x="Week", title = "Delphinium Peak Bloom") +
@@ -34,7 +37,7 @@ ggplot(delph.counts.by.site,aes(x=date,y=number.conspecifics,group=plot.treat)) 
                limits = as.Date(c('2019-06-18','2019-07-25')),
                labels = date_format(format = "%W"))
 
-#plotting Potentilla flower counts
+# plotting Potentilla flower counts
 ggplot(pot.counts.by.site,aes(x=date,y=number.conspecifics,group=plot.treat)) +
   theme_classic() +
   labs(y="Floral Abundance",x="Week", title = "Potentilla Peak Bloom") +
@@ -47,14 +50,15 @@ ggplot(pot.counts.by.site,aes(x=date,y=number.conspecifics,group=plot.treat)) +
                limits = as.Date(c('2019-07-11','2019-08-25')),
                labels = date_format(format = "%W"))
 
-#plotting all together
+# Plotting all species together
 
+# selecting species and combining
 mert.counts.by.site<-mert.counts.by.site %>% mutate(species="Mertensia")
 delph.counts.by.site<-delph.counts.by.site%>% mutate(species="Delphinium")
 pot.counts.by.site<-pot.counts.by.site %>% mutate(species="Potentilla")
 all.spec.bloom<-rbind(mert.counts.by.site,delph.counts.by.site,pot.counts.by.site)
 
-
+# plotting all species
 ggplot(all.spec.bloom,aes(x=date,y=number.conspecifics,
                           group=interaction(plot.treat,species),color=species)) +
   theme_classic() +
@@ -69,9 +73,9 @@ ggplot(all.spec.bloom,aes(x=date,y=number.conspecifics,
                labels = date_format(format = "%W"))
 
 
-### By site
+### Plotting floral abundance by site
 
-#plotting Mertensia flower counts 
+# plotting Mertensia flower counts 
 ggplot(mert.counts.by.site,aes(x=date,y=number.conspecifics,group=site)) +
   theme_classic() +
   labs(y="Floral Abundance",x="Week",title = "Mertensia Peak Bloom") +
@@ -84,7 +88,7 @@ ggplot(mert.counts.by.site,aes(x=date,y=number.conspecifics,group=site)) +
                limits = as.Date(c('2019-06-07','2019-07-18')),
                labels = date_format(format = "%W"))
 
-#plotting Delphinium flower counts
+# plotting Delphinium flower counts
 ggplot(delph.counts.by.site,aes(x=date,y=number.conspecifics,group=site)) +
   theme_classic() +
   labs(y="Floral Abundance",x="Week", title = "Delphinium Peak Bloom") +
@@ -97,7 +101,7 @@ ggplot(delph.counts.by.site,aes(x=date,y=number.conspecifics,group=site)) +
                limits = as.Date(c('2019-06-18','2019-07-25')),
                labels = date_format(format = "%W"))
 
-#plotting Potentilla flower counts
+# plotting Potentilla flower counts
 ggplot(pot.counts.by.site,aes(x=date,y=number.conspecifics,group=site)) +
   theme_classic() +
   labs(y="Floral Abundance",x="Week", title = "Potentilla Peak Bloom") +
@@ -110,37 +114,41 @@ ggplot(pot.counts.by.site,aes(x=date,y=number.conspecifics,group=site)) +
                limits = as.Date(c('2019-07-11','2019-08-25')),
                labels = date_format(format = "%W"))
 
-#plotting all sites/treatments together
-#Mertensia
+### Plotting all sites/treatments together
+
+# Mertensia
 ggplot(mert.counts.by.site,aes(x=date,y=number.conspecifics)) +
   theme_classic() +
   geom_point() +
   geom_smooth(se=FALSE,color="darkgreen") +
   geom_vline(xintercept=as.Date("2019-06-27")) +
   geom_vline(xintercept = as.Date("2019-06-20"),linetype="dotdash") +
-  labs(y="Floral Abundance",x="Week", title = "Mertensia Peak Bloom") +
+  labs(y="Floral Abundance",x="Week", title = "Mertensia") +
+  theme(plot.title = element_text(face="italic"))+
   scale_color_brewer(palette="Dark2") +
   scale_x_date(date_breaks = "1 week",
                limits = as.Date(c('2019-06-07','2019-07-18')),
                labels = date_format(format = "%W"))
-#Delphinium
+# Delphinium
 ggplot(delph.counts.by.site,aes(x=date,y=number.conspecifics)) +
   theme_classic() +
   geom_point() +
   geom_smooth(se=FALSE,color="darkgreen") +
   geom_vline(xintercept=as.Date("2019-07-06")) +
-  labs(y="Floral Abundance",x="Week", title = "Delphinium Peak Bloom") +
+  labs(y="Floral Abundance",x="Week", title = "Delphinium") +
+  theme(plot.title = element_text(face="italic"))+
   scale_color_brewer(palette="Dark2") +
   scale_x_date(date_breaks = "1 week",
                limits = as.Date(c('2019-06-18','2019-07-25')),
                labels = date_format(format = "%W"))
-#Potentilla
+# Potentilla
 ggplot(pot.counts.by.site,aes(x=date,y=number.conspecifics)) +
   theme_classic() +
   geom_point() +
   geom_smooth(se=FALSE,color="darkgreen") +
   geom_vline(xintercept=as.Date("2019-08-02")) +
-  labs(y="Floral Abundance",x="Week", title = "Potentilla Peak Bloom") +
+  labs(y="Floral Abundance",x="Week", title = "Potentilla") +
+  theme(plot.title = element_text(face="italic"))+
   scale_color_brewer(palette="Dark2") +
   scale_x_date(date_breaks = "1 week",
                limits = as.Date(c('2019-07-11','2019-08-25')),
